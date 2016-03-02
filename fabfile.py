ea9917c53dbadc474e75279c9b52848b2d285b07
Alastair Porter <alastair@porter.net.nz>
2016-03-02 16:13:05 +0100

    add ssh command to fabric

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,6 @@
 from fabric.api import local
 from fabric.colors import green
+import os
 
 def git_pull():
     local("git pull origin")
@@ -9,6 +10,16 @@ def git_pull():
 def install_requirements():
     local("pip install -r requirements.txt")
     print(green("Installed requirements.", bold=True))
+
+
+def vssh():
+    """SSH to a running vagrant host."""
+    curdir = os.path.dirname(os.path.abspath(__file__))
+    configfile = os.path.join(curdir, '.vagrant', 'ssh_config')
+    if not os.path.exists(configfile):
+        local('vagrant ssh-config metadb > .vagrant/ssh_config')
+
+    local("ssh -F .vagrant/ssh_config -o 'ControlMaster auto' -o 'ControlPath ~/.ssh/mdb_vagrant_control' -o 'ControlPersist 4h' metadb")
 
 
 def deploy():

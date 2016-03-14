5c67c940832c0133ac5253c42bb7ee10077e57f5
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2016-03-14 19:36:25 +0100

    Update lookup script, update discogs scraper
    - Make script more generic
    - Add command-line options for querying
    - Add a mode to run queries based on a csv file
    - Write results to files, skip scraping mbid if file exist
    - Move scrapers to /release and /recording folders according to their type
    - Write Discogs scraper for releases. Match releases by artist name,
      release title, and release year

---
+++ metadb/scrapers/release/discogs.py
@@ -0,0 +1,43 @@
+import json
+import re
+import discogs_client
+import time
+
+TYPE = "release"
+
+DISCOGS_KEY = ""
+
+class ApiException(Exception):
+    pass
+
+
+def scrape(query):
+
+    mbid = query['mbid']
+    artist = query['artist']
+    release = query['release']
+    year = query['year']
+
+    if not artist or not release or not year:
+        raise Exception("Artist, release, and year information is required")
+    print "Scraping recording information for", mbid + ":", artist, "-", release, "-", year
+    artist = artist.decode('utf8')
+    release = release.decode('utf8')
+
+    release_simple = re.sub(r'\W+', '', release.lower())
+    year = year.split('-')[0]
+
+    data = None
+    try:
+        d = discogs_client.Client('AB_Discogs/0.1', user_token=DISCOGS_KEY)
+        results = d.search(artist=artist, release_title=release)
+
+        data = [r.data for r in results if 'year' in r.data and r.data['year']==year]
+
+    except ApiException as ex:
+        raise
+
+    if data:
+        return data, TYPE
+    else:
+        return None, None

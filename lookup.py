809f7b8855ef19e88e3248ee3c564414ce6f72c4
Alastair Porter <alastair@porter.net.nz>
2016-03-15 13:48:31 +0100

    Separate data from configuration

--- lookup.py
+++ lookup.py
@@ -10,75 +10,63 @@ def _get_module_by_path(modulepath):
     try:
         package = importlib.import_module(modulepath)
         return package
-    except ImportError:
-        raise Exception("Cannot load module %s" % modulepath)
+    except ImportError as e:
+        raise Exception("Cannot load module %s (%s)" % (modulepath, e))
 
 
-def save(result, outfile):
+def save_result(result, outfile):
     with open(outfile, 'w') as f:
         json.dump(result, f)
 
 
 def process_file(module, filename, save=False):
     with open(filename) as csvfile:
-        for query in csv.DictReader(csvfile):
-            if not 'module' in query:
-                query['module'] = module
-            if not 'save' in query:
-                query['save'] = save
+        for row in csv.DictReader(csvfile):
+            if not 'year' in row:
+                row['year'] = None
+            if not 'artist' in row:
+                row['artist'] = None
+            if not 'recording' in row:
+                row['recording'] = None
+            if not 'mbid' in row:
+                row['mbid'] = None
+
+            process(module, row, save)
 
 
-            if not 'year' in query:
-                query['year'] = None
-            if not 'artist' in query:
-                query['artist'] = None
-            if not 'recording' in query:
-                query['recording'] = None
-            if not 'mbid' in query:
-                query['mbid'] = None
+def process(module_path, data, save):
+    if not module_path:
+        raise Exception("Missing module information")
 
-            process(query)
-
-
-def process(query):  
-
-    if not 'module' in query or not query['module']:
-        raise Exception("Missing module information for the query", json.dumps(query))
-
-    module = _get_module_by_path(query['module'])
+    module = _get_module_by_path(module_path)
 
     if not hasattr(module, "scrape"):
         raise Exception("Module %s must have a .scrape method" % module)
 
-    if not query['mbid']:
-        raise Exception("Missing MBID for the query", json.dumps(query))
+    if not data['mbid']:
+        raise Exception("Missing MBID for the query", json.dumps(data))
 
-    if query['save']:
+    if save:
         # Check if result file already exists
-        outfile = query['mbid'] + '.json'
+        outfile = data['mbid'] + '.json'
         if os.path.exists(outfile):
             print "File", outfile, "found, skipping query"
             return
     try:
-        result, result_type = module.scrape(query)
+        result, result_type = module.scrape(data)
     except Exception, e:
         print str(e)
         return
 
-    # Save empty results too
-    
-    #if not result:
-    #    return
-        
     result = {
                 'type': result_type,
-                'mbid': query['mbid'],
-                'scraper': query['module'],
+                'mbid': data['mbid'],
+                'scraper': module_path,
                 'result': result
              }
 
-    if query['save']:
-        save(result, outfile)
+    if save:
+        save_result(result, outfile)
     else:
         print result
         print
@@ -88,7 +76,7 @@ if __name__ == "__main__":
     parser = ArgumentParser(description = """
 MetaDB metadata scraper.
 """)
-    parser.add_argument('--module', help='Scraper module python path, e.g. metadb.scrapers.lastfm', required=False)
+    parser.add_argument('--module', help='Scraper module python path, e.g. metadb.scrapers.lastfm', required=True)
     parser.add_argument('--csv', help='Use input csv file for queries', required=False)
     parser.add_argument('--artist', help='Artist name', required=False)
     parser.add_argument('--recording', help='Recording title', required=False)
@@ -100,9 +88,15 @@ MetaDB metadata scraper.
     args = parser.parse_args()
 
     if args.csv:
-        if args.artist or args.recording or args.release or args.mbid: 
+        if args.artist or args.recording or args.release or args.mbid:
             print 'Performing queries using data in ', args.csv, 'file; --artist/--recording/--release/--mbid flags will be ignored'
         process_file(args.module, args.csv, args.save)
 
     else:
-        process(args.__dict__)
+        query = {"mbid": args.mbid,
+                 "recording": args.recording,
+                 "release": args.release,
+                 "year": args.year,
+                 "artist": args.artist
+                }
+        process(args.module, query, args.save)

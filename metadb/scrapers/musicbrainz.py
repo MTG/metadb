932780020dcbec70b1beb23fc37b505ba531a820
kosmasK <kosmas.kritsis@gmail.com>
2016-03-20 19:36:13 +0100

    MB scraper - artists and their MBID added

--- metadb/scrapers/musicbrainz.py
+++ metadb/scrapers/musicbrainz.py
@@ -1,10 +1,11 @@
-import getpass as gt
+# import getpass as gt
 import sys
 import musicbrainzngs as mb
 
-username = raw_input('Enter MusicBrainz Username: ')
-password = gt.getpass('Password for {}: '.format(username))
-mb.auth(username,password)
+# Username and password promting
+# username = raw_input('Enter MusicBrainz Username: ')
+# password = gt.getpass('Password for {}: '.format(username))
+# mb.auth(username,password)
 
 mb.set_useragent(
     "python-metadb-musicbrainz-scrape",
@@ -13,21 +14,25 @@ mb.set_useragent(
 
 
 
-def scrape(mbid, artist, title):
+def scrape(mbid):
     try:
-        resultID = mb.get_recording_by_id(mbid,
-                                          includes=["artists", "releases", "annotation", "tags"],
-                                          release_status=["official"],
-                                          release_type=["album", "single", "ep", "broadcast", "other", "compilation"])
+        result = mb.get_recording_by_id(mbid, includes=["artists", "releases", "tags"])
+
+        artists = list()
+        artists_mbid = list()
+
+        for item in result["recording"]["artist-credit"]:
+            if isinstance(item, dict):
         
-        resultArtist = mb.search_artists(artist=artist,
-                                         limit = 10,
-                                         strict = True)
+                artists.append(item["artist"]["name"])
+                artists_mbid.append(item["artist"]["id"])
+
+        
+        #resultArtist = mb.search_artists(artist=artist, limit = 10, strict = True)
     
-        resultTitle = mb.search_recordings(recording=title,
-                                           limit = 10,
-                                           strict = True)
+        #resultTitle = mb.search_recordings(recording=title, limit = 10, strict = True)
+
     except mb.MusicBrainzError as ex:
         raise
     
-    return resultID, resultArtist, resultTitle
+    return artists,artists_mbid

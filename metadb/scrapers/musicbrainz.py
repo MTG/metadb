0463d36c0efe459fe47dd780d82821e89fa6cb61
kosmasK <kosmas.kritsis@gmail.com>
2016-03-22 21:42:56 +0100

    Tags added but tags missing! mb python lib error

--- metadb/scrapers/musicbrainz.py
+++ metadb/scrapers/musicbrainz.py
@@ -15,29 +15,86 @@ def scrape(mbid):
     try:
         recording = mb.get_recording_by_id(mbid, includes=["artists", "releases", "tags"])
         
-        recording_name = recording["recording"]["title"] #recording title
+        # Recording title
+        recording_name = recording["recording"]["title"]
         
         artists = []
-        
-        for item in recording["recording"]["artist-credit"]:
-            if isinstance(item, dict):
-                artists.append({"id": item["artist"]["id"], "name": item["artist"]["name"]})
-        
         releases = []
         release_groups = []
         
+        
+        for item in recording["recording"]["artist-credit"]:
+            # Condition to skip join-phrase in multi-artists recordings
+            if isinstance(item, dict):
+                
+                # Recording artist info
+                artist_id = item["artist"]["id"]
+                artist_name = item["artist"]["name"]
+                # Check for recording artist tag-list
+                if "tag-list" in item["artist"]:
+                    artist_tags = item["artist"]["tag-list"]
+                    artists.append({"id": artist_id, "name": artist_name, "tag-list": artist_tags})
+                else:
+                    artists.append({"id": artist_id, "name": artist_name})
+        
+        
         for item in recording["recording"]["release-list"]:
-            releases.append({"id": item["id"], "title": item["title"], "date": item["date"]})
             
-            #Request for retrieving release info
+            # Request for release info --- TAGS include is not SUPPORTED in the get_release_by_id method????????
+            # release = mb.get_release_by_id(item["id"], includes = ["artists", "release-groups", "tags"])
             release = mb.get_release_by_id(item["id"], includes = ["artists", "release-groups"])
-            release_group = {"id": release["release"]["release-group"]["id"], "title": release["release"]["release-group"]["title"]}
             
-            #Check if release group already exists in the list in order to eliminate duplicate instances
+            # Release group info
+            release_group_id = release["release"]["release-group"]["id"]
+            release_group_title = release["release"]["release-group"]["title"]
+            release_group_first_release_date = release["release"]["release-group"]["first-release-date"]
+            
+            release_artists = []
+            
+            for item2 in release["release"]["artist-credit"]:
+                # Condition to skip join-phrase in multi-artists releases
+                if isinstance(item2, dict):
+                    # Release artist info
+                    release_artist_id = item2["artist"]["id"]
+                    release_artist_name = item2["artist"]["name"]
+                    
+                    # Check for artist tag-list
+                    if "tag-list" in item2["artist"]:
+                        release_artist_tags = item2["artist"]["tag-list"]
+                        release_artists.append({"id": release_artist_id, "name": release_artist_name, "tag-list": release_artist_tags})
+                    else:
+                        release_artists.append({"id": release_artist_id, "name": release_artist_name})
+            
+            
+            
+            # Release group tags check and first-release-date
+            if "tag-list" in release["release"]["release-group"]:
+                release_group_tags = release["release"]["release-group"]["tag-list"]
+                release_group = {"id": release_group_id, "title": release_group_title, "firs-release-date": release_group_first_release_date, "tag-list": release_group_tags}
+            else:
+                release_group = {"id": release_group_id, "title": release_group_title, "firs-release-date": release_group_first_release_date}
+            
+            # Check if release group already stored - eliminate dublicates
             if release_group not in release_groups:
                 release_groups.append(release_group)
             
+            
+            # Check for release tag-list
+            if "tag-list" in release["release"]:
+                release_tags = release["release"]["tag-list"]
+                releases.append({"id": item["id"], "title": item["title"], "artist": release_artists, "date": item["date"], "tag-list": release_tags})
+            else:
+                releases.append({"id": item["id"], "title": item["title"], "artist": release_artists, "date": item["date"]})
+
+
     except mb.MusicBrainzError as ex:
         raise
     
-    return {"title": recording_name, "artist": artists, "releases": releases, "realease-groups": release_groups}
+    # Final result based on the existance of recording tags
+    if "tag-list" in recording["recording"]:
+        recording_tags = recording["recording"]["tag-list"]
+        result = {"title": recording_name, "artist": artists, "releases": releases, "realease-groups": release_groups, "tag-list": recording_tags}
+    else:
+        result = {"title": recording_name, "artist": artists, "releases": releases, "realease-groups": release_groups}
+    
+    return result

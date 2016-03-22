18525bcc4ebfbc03aa5d3c65ea7dc9893436f205
kosmasK <kosmas.kritsis@gmail.com>
2016-03-22 03:47:41 +0100

    MB Scraper updated: Release-groups added

--- metadb/scrapers/musicbrainz.py
+++ metadb/scrapers/musicbrainz.py
@@ -11,33 +11,34 @@ mb.set_useragent(
     "0.1",
     "https://github.com/MTG/metadb")
 
-
-
 def scrape(mbid):
     try:
         result = mb.get_recording_by_id(mbid, includes=["artists", "releases", "tags"])
-	
-	recordingName = result["recording"]["title"] #recording title
-	
-	artist = list()
-	artist_mbid = list()
-
-	for item in result["recording"]["artist-credit"]:
-    		if isinstance(item, dict): 
-        		artist.append(item["artist"]["name"])
-        		artist_mbid.append(item["artist"]["id"])
-			
-
-        release_mbid = list()
-	release_title = list()
-
-	for item in result["recording"]["release-list"]:
-    		release_mbid.append(item["id"])
-    		release_title.append(item["title"])
-
-
-
+        
+        recording_name = result["recording"]["title"] #recording title
+        
+        artists = []
+        
+        for item in result["recording"]["artist-credit"]:
+            if isinstance(item, dict):
+                artists.append({"id": item["artist"]["id"], "name": item["artist"]["name"]})
+        
+        releases = []
+        release_groups = []
+        
+        for item in result1["recording"]["release-list"]:
+            releases.append({"id": item["id"], "Title": item["title"], "Date": item["date"]})
+            
+            #Request for retrieving release info
+            rel = mb.get_release_by_id(item["id"], includes = ["artists", "release-groups"])
+            
+            #Check if release group already exists in the list in order to eliminate dublicate instances
+            if not ({"id": rel["release"]["release-group"]["id"], "Title": rel["release"]["release-group"]["title"]}) in release_groups:
+                release_groups.append({"id": rel["release"]["release-group"]["id"], "Title": rel["release"]["release-group"]["title"]})
+            else:
+                print "Release-group already exists in the list!"
+  
     except mb.MusicBrainzError as ex:
         raise
     
-return recordingName, artist, artist_mbid, release_mbid, release_title
+return {"Title": recording_name, "Artist": artists, "Releases": releases, "Realease-groups": release_groups}

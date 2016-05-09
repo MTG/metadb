24d95fad59a6d64334d2e10a8926582b56f2d209
Alastair Porter <alastair.porter@upf.edu>
2016-05-09 16:41:48 +0200

    python3 setup steps

--- admin/setup_app.sh
+++ admin/setup_app.sh
@@ -5,9 +5,10 @@ if [ $# -eq 1 ]
 else
     echo "Application directory is not specified. Using current directory!"
 fi
-echo "source venv-metadb/bin/activate" > ~/.bashrc
-virtualenv ../venv-metadb
+echo "source venv-metadb/bin/activate" >> ~/.bashrc
+virtualenv -p python3 ../venv-metadb
 source ../venv-metadb/bin/activate
-pip install -r requirements.txt
+pip install -r requirements
+pip install -r requirements_dev
 python manage.py init_db
 python manage.py init_test_db

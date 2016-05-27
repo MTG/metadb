9540b7916b75121bb81c1ab68f7d184ccf7fd74d
Alastair Porter <alastair.porter@upf.edu>
2016-05-27 23:23:23 +0200

    generate cookie on scrape start

--- metadb/scrapers/release/allmusic.py
+++ metadb/scrapers/release/allmusic.py
@@ -4,6 +4,7 @@ import sys
 import json
 import errno
 from bs4 import BeautifulSoup
+from requests.adapters import HTTPAdapter
 import time
 import random
 
@@ -12,11 +13,21 @@ from metadb import log
 
 TYPE = "release"
 
+session = requests.Session()
+adapter = HTTPAdapter(max_retries=5, pool_connections=100, pool_maxsize=100)
+session.mount("http://www.allmusic.com", adapter)
+
+session_cookies = {}
+
 def _get_cookies():
-    r = requests.get("http://www.allmusic.com/")
-    return r.cookies
+    headers = query_data()
+    r = session.get("http://www.allmusic.com/", headers=headers)
+    session_cookies['allmusic_session'] = r.cookies.get('allmusic_session')
 
 def scrape(query):
+    if not session_cookies:
+        _get_cookies()
+
     mbid = query['mbid']
     artist = query['artist']
     release = query['release']
@@ -36,14 +47,18 @@ def scrape(query):
     data["search_results"] = searchres
     data["album"] = {}
     if searchres:
-        relres = get_first_release(searchres)
+        relres = get_first_release(searchres, year)
         release = parse_release(relres)
         data["album"] = release
 
     return {"type": TYPE, "data": data}
 
-def get_first_release(data):
+def get_first_release(data, year):
     if data:
+        if year:
+            filtered = [d for d in data if d["year"] == year]
+            if filtered:
+                data = filtered
         rel = data[0]
         releaseurl = rel["albumdata"].get("href")
         if releaseurl:
@@ -84,13 +99,13 @@ def parse_release(data):
 
 
 def load_release(url):
-    cookies, headers = query_data()
-    r = requests.get(url, headers=headers, cookies=cookies)
+    headers = query_data()
+    r = session.get(url, headers=headers, cookies=session_cookies)
     return r.content
 
 def search(artist, release):
-    cookies, headers = query_data()
-    r = requests.get('http://www.allmusic.com/search/albums/{} {}'.format(artist, release), headers=headers, cookies=cookies)
+    headers = query_data()
+    r = session.get('http://www.allmusic.com/search/albums/{} {}'.format(artist, release), headers=headers, cookies=session_cookies)
     return r.content
 
 def parse_search(data, artist, release):
@@ -129,22 +144,6 @@ def parse_search(data, artist, release):
 
 
 def query_data():
-    cookies = {
-        '_gat': '1',
-        '__qca': 'P0-559467384-1457973982414',
-        '_gat_cToolbarTracker': '1',
-        'bm_monthly_unique': 'true',
-        'bm_daily_unique': 'true',
-        'bm_sample_frequency': '1',
-        'policy': 'notified',
-        'registration_prompt': '3',
-        'allmusic_session': 'a%3A6%3A%7Bs%3A10%3A%22session_id%22%3Bs%3A32%3A%2268b730334cd74b640a5b17dd563b2b61%22%3Bs%3A10%3A%22ip_address%22%3Bs%3A18%3A%222607%3Af700%3A1%3A83%3A%3A92%22%3Bs%3A10%3A%22user_agent%22%3Bs%3A120%3A%22Mozilla%2F5.0+%28Macintosh%3B+Intel+Mac+OS+X+10_11_3%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F50.0.2661.18+Safari%2F537.36%22%3Bs%3A13%3A%22last_activity%22%3Bi%3A1457973975%3Bs%3A9%3A%22user_data%22%3Bs%3A0%3A%22%22%3Bs%3A4%3A%22user%22%3Bi%3A0%3B%7D426c4443986a9d499e0956f67c107ae7776f09c9',
-        '_ga': 'GA1.2.1178384982.1457973977',
-        'ads_bm_last_load_status': 'NOT_BLOCKING',
-        'bm_last_load_status': 'NOT_BLOCKING',
-        'OX_plg': 'pm',
-    }
-
     headers = {
         'DNT': '1',
         'Accept-Encoding': 'gzip, deflate, sdch',
@@ -152,9 +151,8 @@ def query_data():
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.18 Safari/537.36',
         'Accept': '*/*',
         'Referer': 'http://www.allmusic.com/',
-        'X-Requested-With': 'XMLHttpRequest',
         'Connection': 'keep-alive',
         'Cache-Control': 'max-age=0',
     }
-    return cookies, headers
+    return headers
 

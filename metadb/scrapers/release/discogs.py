0caacaa4df679c0f4bae70101ec10fa958f6afa3
Alastair Porter <alastair.porter@upf.edu>
2017-05-09 22:48:45 +0200

    Do processing of discogs data in dump function

--- metadb/scrapers/release/discogs.py
+++ metadb/scrapers/release/discogs.py
@@ -1,9 +1,12 @@
 import json
 import re
 import discogs_client
+import sys
+import yaml
 
 DISCOGS_KEY = ""
 
+DATA = {}
 
 class ApiException(Exception):
     pass
@@ -17,21 +20,57 @@ def dispose():
     pass
 
 
+def config_dump(data_filename):
+    # we use DATA so that we don't have to use global
+    GENRE_TREE = yaml.load(open(data_filename))
+    DATA["GENRE_TREE"] = GENRE_TREE
+    DATA["GENRE_TREE_styles"] = set([s for g in GENRE_TREE for s in GENRE_TREE[g]])
+
+
+def extract_style(styles, genres):
+    # find a parent genre among genres for each style in styles
+    result = []
+    for s in list(set(styles)):
+        # Fix a style that was renamed by Discogs at some point in time
+        s = s.replace("Shoegazer", "Shoegaze")
+
+        #if s not in DATA["GENRE_TREE_styles"]:
+        #   print("Unknown style: {}".format(s))
+
+        matched = False
+        for g in genres:
+            if s in DATA["GENRE_TREE"][g]:
+                result.append([g, s])
+                matched = True
+        #if not matched:
+        #    print("Could not match {} to {}".format(s, genres))
+
+    return result
+
+
 def parse_db_data(mbid, data, writer):
     if not data:
         print('{} nodata'.format(mbid), file=sys.stderr)
-    for r in data:
-        id = r['id']
-        genre = r['genre']
-        style = r['style']
-        type = r['type']
-        if genre:
-            row = [mbid, id, type, 'genre']
-            row.extend(genre)
-            writer.writerow(row)
-        if style:
-            row = [mbid, id, type, 'style']
-            row.extend(style)
+
+    first = data[0]
+    genres = first['genre']
+    styles = first['style']
+
+    if styles:
+        styles = extract_style(styles, genres)
+
+    genres_with_no_styles = set(genres) - set([g for g, s in styles])
+    data = styles + [[g] for g in genres_with_no_styles]
+
+    if isinstance(mbid, list):
+        # We have a list of recording mbids, we should write the genre data once for each mbid
+        for m in mbid:
+            for d in data:
+                row = [m] + d
+                writer.writerow(row)
+    else:
+        for d in data:
+            row = [mbid] + d
             writer.writerow(row)
 
 
